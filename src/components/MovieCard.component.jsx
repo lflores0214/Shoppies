5773bd6ef4c99849fa9d00b1370eafddf729036e
Luis Flores <lflores02142@gmail.com>
2020-09-01 16:05:38 -0700

    basic layout

--- src/components/MovieCard.component.jsx
+++ src/components/MovieCard.component.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 
+
+
 const MovieCard = ({
   title,
   year,
---
+++ src/components/styles/Main.styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const MainContainer = styled.div`
+width: 90%;
+margin-left: 5%;
+padding: 2%;
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color:whitesmoke;
+height: 90vh
+`
+export const SearchContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 1%;
+border: 1px solid white;
+
+`
+
+export const Input = styled.input`
+width: 100%;
+`
+export const ResNomContainer = styled.div`
+width: 90%;
+display: flex;
+justify-content: space-between;
+`
